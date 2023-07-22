2ad035e64475568e0b3a7354669f6c598b3ee467
Kakao1513 <42140061+Kakao1513@users.noreply.github.com>
2023-07-23 03:02:09 +0900

    2023-07-23

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import './App.css';
-import Stack from '@mui/material/Stack'
-import Button from '@mui/material/Button'
-import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid'
-import Dialog from '@mui/material/Dialog';
-import DialogTitle from '@mui/material/DialogTitle';
-import DialogContent from '@mui/material/DialogContent';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContentText from '@mui/material/DialogContentText';
-import { ToggleButton } from '@mui/material';
+import {
+  ToggleButtonGroup, ToggleButton,
+  Stack, Button, Container, Grid, Box,
+  Dialog, DialogTitle, DialogContent, DialogActions, DialogContentText, ButtonGroup
+} from '@mui/material';
 import { useState, useEffect, useRef, Fragment } from 'react';
 import { useDataStore } from './store/store.js';
 
 function Header() {
   return (
-    <header className="App-header">
+    <header className="app-header">
       <div>
         <h4>네이버 뉴스 요약기</h4>
       </div>
@@ -23,40 +18,25 @@ function Header() {
 }
 
 function Navigation() {
-  const [style, setStyle] = useState('outlined');
-
-  const GetClick = (e) => {
-    if (style === 'outlined') {
-      setStyle('contained');
-      setCurrentClick('selected');
-    }
-    else if (style === 'contained') {
-      setStyle('outlined')
-      setPrevClick('selected');
-    }
+  const categori = [
+    { label: '경제' },
+    { label: '정치' },
+    { label: 'IT/기술' },
+    { label: '사회' },
+    { label: '세계' },
+    { label: '생활/문화' },
+  ]
+  const [selected, setSelected] = useState(0);
+  const handleBottonClick = (bindex) => {
+    setSelected(bindex);
   }
-
-  useEffect(
-    (e) => {
-      if (currentClick !== null) {
-        setStyle('contained');
-      }
-
-      if (prevClick !== null) {
-        setStyle('outlined');
-      }
-      setPrevClick(currentClick);
-    }, [style]);
-
   return (
     <Stack spacing={1} direction="row" justifyContent={'center'}>
-      <Button variant={style} onClick={GetClick}>경제</Button>
-      <Button variant={style} onClick={GetClick}>정치</Button>
-      <Button variant={style} onClick={GetClick}>IT/기술</Button>
-      <Button variant={style} onClick={GetClick}>사회</Button>
-      <Button variant={style} onClick={GetClick}>세계</Button>
-      <Button variant={style} onClick={GetClick}>생활/문화</Button>
+      {categori.map((button, index) => (
+        <Button key={index} variant={selected === index ? 'contained' : 'outlined'} onClick={() => handleBottonClick(index)}>{button.label}</Button>
+      ))}
     </Stack>
+
   )
 }
 function Nav() {
@@ -67,10 +47,25 @@ function Nav() {
   )
 }
 function Aside() {
+  const { topics } = useDataStore();
+  function topiclist() {
+    const arr = [];
+    for (let topic of topics) {
+      arr.push(
+        <li className="topic-item"><p className='item'>{topic}</p></li>);
+    }
+    return arr
+  }
   return (
     <aside className="container" id="aside">
-      <span className="rank">1</span>
-      <span className="topic">test</span>
+      <div className="topic-title">
+        <h4>Today Hot Topics</h4>
+      </div>
+      <div className="topic-list">
+        <ol className='rank'>
+          {topiclist()}
+        </ol>
+      </div>
     </aside>
   )
 }
@@ -89,24 +84,24 @@ function Main() {
   )
 }
 function ArticleContent(props) {
+  const { article } = useDataStore();
   let rank = props.rank;
   const [open, setOpen] = useState(false);
   return (
     <Fragment>
       <article className="article-container" onClick={() => { setOpen(true) }}>
         <div className="article-item">
-          <span><img src="firstsig/public/logo192.png"></img></span>
-          <span>
+          <span className='article-wrapper'>
             <div className="content">
               <div className="title"><h3>title test</h3></div>
-              <div className="Main-article">
-                <article>Qui aimes-tu le mieux, homme enigmatique, dis?ton pere, ta merem ta soeurs, ou ton frere?
-                </article>
+              <div className="main-area">
+                <p className="main-area ellipsis">{article}</p>
               </div>
               <div className="time"><p>time</p></div>
             </div>
           </span>
         </div>
+        <span className='image-wrapper'><img clsssName='fitimag' src="img\test.jpg" /></span>
       </article>
       <Modal open={open} setOpen={setOpen} />
     </Fragment>
--- src/store/dataList.js
+++
@@ -1,8 +0,0 @@
-import { create } from 'zustand'
-
-export const useDataStore = create((set) => ({
-  newsTitle: ['강남 맛집 추천', '윤석열 퇴진', '경제 개발 5주년', '뉴스 데이터1', '뉴스데이터2'],
-  count: 0,
-  setNewsTitle: (newsTitle) => set({ newsTitle }),
-  setCount: (count) => set({ count }),
-}));
