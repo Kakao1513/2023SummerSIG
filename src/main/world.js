2077fdba96e86cb080333a345fa9c09ac61ef570
Kakao1513 <42140061+Kakao1513@users.noreply.github.com>
2023-07-24 18:54:41 +0900

    2023-07-24 라우팅 기능 추가. SPA기능 개발완료

---
+++ src/main/world.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Route} from 'react-router-dom';
+import Main from '../components/Main';
+function World(){
+    return(
+        <div>
+            <Main>
+                
+            </Main>
+        </div>
+    )
+}
